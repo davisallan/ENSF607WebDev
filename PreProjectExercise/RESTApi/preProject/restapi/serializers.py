48999d38a26358c96b001460aa50d361357133a7
davisallan <davis.allan@ucalgary.ca>
2020-12-09 14:04:01 -0700

    created serializer for Echo object

---
+++ PreProjectExercise/RESTApi/preProject/restapi/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Echo
+
+
+class EchoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Echo
+        fields = 'message'
--- PreProjectExercise/RESTApi/preProject/restapi/views.py
+++ PreProjectExercise/RESTApi/preProject/restapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import EchoSerializer
+from .models import Echo
+
 
 # Create your views here.
+
+class EchoViewSet(viewsets.ModelViewSet):
+    queryset = Echo.objects.all()
+    serializer_class = EchoSerializer
