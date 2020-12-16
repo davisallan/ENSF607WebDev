dd91dc9127c95e55d8b5ddc6580ab99c472bc213
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-12-16 12:43:57 -0700

    Added file for course information

--- Milestone1/course-outline/src/App.js
+++ Milestone1/course-outline/src/App.js
@@ -1,11 +1,12 @@
 import LearningOutcomeList from "./components/LearningOutcomeList";
 import GradeTable from "./components/GradeTable";
+import CourseInformation from "./components/CourseInformation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <CourseInformation />
         <LearningOutcomeList />
         <GradeTable />
       </header>
---
+++ Milestone1/course-outline/src/components/CourseInformation.js
@@ -0,0 +1,100 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import TextAreaAutosize from "@material-ui/core/TextareaAutosize";
+import Grid from "@material-ui/core/Grid";
+import logo from "./schulich.png";
+
+function Logo() {
+  return (
+    <Grid item>
+      <img src={logo} alt="Schulich" />
+    </Grid>
+  );
+}
+
+function CourseNumber() {
+  return (
+    <Grid item>
+      <TextField
+        variant="filled"
+        label="Course Number"
+        placeholder="E.g. ENSF 409"
+      />
+    </Grid>
+  );
+}
+
+function CourseTitle() {
+  return (
+    <Grid item>
+      <TextField
+        variant="filled"
+        label="Course Title"
+        placeholder="E.g. Principles of Software Development"
+        style={{ width: 392 }}
+      />
+    </Grid>
+  );
+}
+
+function CourseDescription() {
+  return (
+    <TextAreaAutosize
+      style={{ width: 386 }}
+      placeholder="Description of the course"
+      rowsMin={3}
+    />
+  );
+}
+
+function CourseLabel(text) {
+  return (
+    <Grid item>
+      <TextField disabled variant="filled" defaultValue={text} />
+    </Grid>
+  );
+}
+
+function CourseInput(text) {
+  return (
+    <Grid item>
+      <TextField variant="filled" placeholder={text} />
+    </Grid>
+  );
+}
+
+export default function CourseInformation() {
+  return (
+    <div>
+      <Grid container>
+        <Logo />
+      </Grid>
+      <Grid container>
+        <h3>1. Calendar Information</h3>
+      </Grid>
+      <Grid container>
+        <CourseNumber />
+      </Grid>
+      <Grid container>
+        <CourseTitle />
+      </Grid>
+      <Grid container>
+        <CourseDescription />
+      </Grid>
+      <Grid container>
+        {CourseLabel("Course Hours:")}
+        {CourseInput("E.g. 3 units; H (3-2)")}
+      </Grid>
+      <Grid container>
+        {CourseLabel("Academic Credit:")}
+        {CourseInput("E.g. 3")}
+      </Grid>
+      <Grid container>
+        {CourseLabel("Calendar Reference:")}
+        {CourseInput(
+          "E.g. http://www.ucalgary.ca/pubs/calendar/current/software-engineering-for-engineers.html#38252"
+        )}
+      </Grid>
+    </div>
+  );
+}
